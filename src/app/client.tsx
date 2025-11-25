d94f2a96612cf8da11cda35ef444365e1650b8f8
Shivam Sood <shivamsood634@gmail.com>
2025-11-26 00:18:09 +0530

    04: inngest setup
    Signed-off-by: Shivam Sood <shivamsood634@gmail.com>

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,12 +1,25 @@
 "use client"
-import { useSuspenseQuery } from "@tanstack/react-query"
+import { useSuspenseQuery, useMutation } from "@tanstack/react-query"
 import { useTRPC } from "@/trpc/client"
+import { toast } from "sonner"
+import { Button } from "@/components/ui/button"
+
 export const Client = () => {
     const trpc = useTRPC()
+    const invoke = useMutation(trpc.invoke.mutationOptions({
+        onSuccess: () => {
+            toast.success("Background process started!")
+        }
+    }))
     const { data } = useSuspenseQuery(trpc.hello.queryOptions({ text: 'from TRPC' }))
 
-    return(
-        <div>
+    return (
+        <div className="p-4 max-w-7xl mx-auto">
+            <Button disabled={invoke.isPending} onClick={() => {
+                invoke.mutate({ text: "emailAddress" })
+            }}>
+                Invoke Inngest Function
+            </Button>
             {JSON.stringify(data)}
         </div>
     )
