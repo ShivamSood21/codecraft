e292c73826c82b232d9bfb5c9d7f1ae215ef67f2
Shivam Sood <shivamsood634@gmail.com>
2025-11-26 00:27:29 +0530

    made changes
    Signed-off-by: Shivam Sood <shivamsood634@gmail.com>

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -5,7 +5,7 @@ export const helloWorld = inngest.createFunction(
   { event: "test/hello.world" },
   async ({ event, step }) => {
     await step.sleep("Download", "20s");
-    await step.sleep("Transcibe", "5s");
+    await step.sleep("Transcribe", "5s");
     await step.sleep("Summarise", "5s");
     return { message: `Hello ${event.data.email}!` };
   },
